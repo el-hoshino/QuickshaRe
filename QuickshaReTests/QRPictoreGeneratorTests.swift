f9de1274131477ec5c05d68ea4335bf70ab71a43
星野恵瑠 <shonetesmith@gmail.com>
2020-01-11 21:31:01 +0900

    Make tests for QRPictoreGenerator

---
+++ QuickshaReTests/QRPictoreGeneratorTests.swift
@@ -0,0 +1,26 @@
+//
+//  QRPictoreGeneratorTests.swift
+//  QRPictoreGeneratorTests
+//
+//  Created by 史翔新 on 2019/12/17.
+//  Copyright © 2019 Crazism. All rights reserved.
+//
+
+import XCTest
+import CoreImage
+@testable import QuickshaRe
+
+class QRPictoreGeneratorTests: XCTestCase {
+    
+    func testPictureGeneration() {
+        
+        let message = "abc"
+        let generator = QRPictureGenerator()
+        let picture = generator.qrPicture(for: message)
+        
+        let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil)!
+        let features = detector.features(in: CIImage(image: picture.uiImage)!) as! [CIQRCodeFeature]
+        XCTAssertEqual(features[0].messageString, message)
+    }
+    
+}
