02ac83b019dc887d85d3df8a45488cfa4ddb0963
史 翔新 <x_shi@yumemi.co.jp>
2020-06-26 00:54:57 +0900

    Remove the workaround which is no longer needed

--- QuickshaRe/SceneDelegate.swift
+++ QuickshaRe/SceneDelegate.swift
@@ -24,7 +24,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             TextInputView()
             Text("Input text from navigation bar to generate QR code image 😘")
         })
-            .workaroundOnRegularSizeClass()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
@@ -64,15 +63,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
 
-}
-
-private extension NavigationView {
-    
-    // Seems that currently SwiftUI has a bug that it won't show master view by default
-    // So here's a workaround↓
-    // Ref: https://stackoverflow.com/questions/57888032/swiftui-navigation-on-ipad-how-to-show-master-list
-    func workaroundOnRegularSizeClass() -> some View {
-        return navigationViewStyle(DoubleColumnNavigationViewStyle()).padding()
-    }
-    
 }
