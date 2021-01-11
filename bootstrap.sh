bbbce4d992ac27b94e01a01b79607f31705577b1
星野恵瑠 <el.hoshino@me.com>
2021-01-11 19:34:44 +0900

    Update bootstrap.sh to remove Mint folder after install finished

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,10 @@
 # Install mint if needed
 which mint > /dev/null || {
   git clone https://github.com/yonaskolb/Mint.git
-  cd Mint
+  pushd Mint
   swift run mint install yonaskolb/mint
-  cd ../
+  popd
+  rm -rf Mint
 }
 
 # Install dependencies via mint
