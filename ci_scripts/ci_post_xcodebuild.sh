ed2ae9a9bcdeabf431704acb1534cd4308481f58
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-06-13 03:36:37 +0900

    Use local danger-js for danger-swift

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -3,4 +3,4 @@
 source ci_variables.sh
 
 cd ${DANGER_PATH}
-swift run danger-swift ci --cwd ${PROJECT_PATH}
+swift run danger-swift ci --danger-js-path node_modules/.bin/danger --cwd ${PROJECT_PATH}
