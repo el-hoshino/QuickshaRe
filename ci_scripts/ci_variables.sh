869db4e38fc78df3c47aed3e69534a93d3041120
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2023-01-11 23:59:19 +0900

    Add a link to Danger in ci_scripts

--- ci_scripts/ci_variables.sh
+++ ci_scripts/ci_variables.sh
@@ -25,7 +25,7 @@ danger_install() {
 }
 
 danger_run() {
-    ls -al "${CI_PROJECT_FILE_PATH}/../" ## test
-    pushd ${DANGER_PATH}
+    ls -al Danger ## test
+    pushd Danger
     swift run danger-swift ci --danger-js-path node_modules/.bin/danger --cwd ${PROJECT_PATH}
 }
