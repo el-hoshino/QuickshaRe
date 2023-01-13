5409a6e7649c1a3294e54ec7a7d5bdef5b56d898
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2023-01-14 00:22:33 +0900

    Remove macOS support in AppPackage

--- AppPackage/Package.swift
+++ AppPackage/Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "AppPackage",
     platforms: [
         .iOS(.v14),
-        .macOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
