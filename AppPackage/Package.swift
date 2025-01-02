5bfa4eb8a0dd198b53687e902a4662a0df532b99
lovee <3942121+el-hoshino@users.noreply.github.com>
2025-01-03 00:27:13 +0900

    Migrate app to iOS 17 so we can use Observation

--- AppPackage/Package.swift
+++ AppPackage/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AppPackage",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
